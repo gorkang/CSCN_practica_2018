eda7f1093116266c9d5969bb79bce1a1097edfc4
Nicomero <nicolas.alarcon.14@sansano.usm.cl>
2018-01-09 16:57:05 -0300

    instrucciones

---
+++ Modulos/bayes/experiment.js
@@ -0,0 +1,20 @@
+
+
+var mainexplanation={
+    type: "instructions",
+    pages: ["<div class = centerbox>"+
+           "<p class = center-block-text>"+
+           "Gracias por participar en este estudio.<br /><br />"+
+           "A continuaci&oacute;n te presentaremos una serie de preguntas, mediante textos, im&aacute;genes o audio.<br /><br />"+
+           "Cada bloque de preguntas tiene sus propias instrucciones, que ver&aacute;s al inicio de cada uno.<br /><br />"+
+           "Para empezar, haz click en el siguiente bot&oacute;n.<br />"+
+           "</p></div>"],
+    allow_keys: false,
+    show_clickable_nav: true,
+    timing_post_trial: 50,
+    data:{trialid: "Welcome_Screen"}
+};
+
+var bayes_experiment = [];
+
+bayes_experiment.push(mainexplanation);
