08bbae31af52a52a8cf3b1ab78622ba554ed7a42
Alejandro Cofre Garcia <fethrblaka13@gmail.com>
2018-01-15 15:21:30 -0300

    Updated style to 25% margin and left align.

--- bayes/experiment.js
+++ bayes/experiment.js
@@ -274,7 +274,7 @@ function createTrial(){//accordig to response
             console.log(temp);
             var typeTrial = {
                 type : "survey-multi-choice",
-                questions: [{prompt: prompts[i], options:temp, required:true}],
+                questions: [{prompt: prompts[i], options:temp, required:true, horizontal:true}],
                 on_finish: function(data) {
                     console.log("QL");
                 }
