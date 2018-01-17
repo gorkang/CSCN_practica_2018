fd599302449646d54158c7abf99d539dce435c51
Alejandro Cofre Garcia <fethrblaka13@gmail.com>
2018-01-17 10:33:11 -0300

    start improved

--- start.py
+++ start.py
@@ -1,6 +1,9 @@
 import subprocess
+import webbrowser
 import signal
+import pygtk
 import time
+import gtk
 import csv
 import os
 
@@ -20,12 +23,13 @@ def stop_handler(signumber, frame):
     else:
         print("The experiment finished successfully.")
         subprocess.call(["docker", "rm", container])
-        subprocess.call(["docker", "rmi", image])
+        subprocess.call(["docker", "rmi", image, "--force"])
         os.remove("run_incomplete")
     exit()
 
 if(not os.path.isfile(imagename) or not os.path.isfile(filename)):
-    subprocess.call(["./dropbox_uploader.sh","download",imagename])
+    #subprocess.call(["./dropbox_uploader.sh","download",imagename + ".gz"])
+    #subprocess.call(["gzip","-d", imagename + ".gz"])
     subprocess.call(["./dropbox_uploader.sh","download",filename])
 if(not os.path.isfile(imagename) or  not os.path.isfile(filename)):
     if(os.path.isfile("Dockerfile")):
@@ -34,19 +38,21 @@ if(not os.path.isfile(imagename) or  not os.path.isfile(filename)):
         os.remove("startscript.sh")
     subprocess.call(("docker run -v " + os.getcwd() + ":/data vanessa/expfactory-builder build /data/ansiedad_matematica /data/comprension_lectora /data/habilidad_matematica /data/memoria_funcional /data/rotacion_mental /data/crtnum /data/crt_verbal /data/graph_literacy /data/matrices /data/numeracy /data/bayes").split())
     print("Compiling...")
-    image = subprocess.check_output("docker build --quiet -t expfactory/experiments .".split())[7:-1]#add --no-cache
-    container = subprocess.check_output(["docker", "run", "-d", image, "start"])[:-1]
+    image = subprocess.check_output("docker build --no-cache --quiet -t expfactory/experiments .".split())[7:-1]
+    container = subprocess.check_output(["docker", "run", "-d", image, "-v", os.path.realpath("") + "data:/scif/data/", "start"])[:-1]
     file = open(filename,'w')
     file.write(subprocess.check_output(["docker", "exec", container, "expfactory", "users", "--new", str(input("Input number of runs:"))]))
     file.close()
-    print "tsv generado."
+    print "Tsv generated."
     #subprocess.call(randomizer)
     subprocess.call(["docker", "cp", filename, container + ":/scif/apps/bayes/" + filename])
-    subprocess.call(["docker", "stop", container])
     image = subprocess.check_output(["docker", "commit", container])[7:-1]
-    print("Creating experiments.tar")
+    subprocess.call(["docker", "stop", container])
+    print("Creating experiments.tar.")
     subprocess.call(["docker", "save", "--output", imagename, image])
-    subprocess.call(["./dropbox_uploader.sh","upload",imagename, ""])
+    print("Compresing.")
+    #subprocess.call(["gzip", "--fast", "-k", imagename])
+    #subprocess.call(["./dropbox_uploader.sh","upload",imagename + ".gz", ""])
     subprocess.call(["./dropbox_uploader.sh","upload",filename, ""])
 print("Loading...")
 image = subprocess.check_output(["docker", "load", "--input", imagename])[24:-1]
@@ -63,8 +69,6 @@ else:
     for row in tsv:
         if(row[1][-7:-1] == "active" and token == ""):
             token = row[1][:-8]
-            token_list = subprocess.check_output(["docker", "exec", container, "expfactory", "users", "--list"]).split()
-            subprocess.call(["docker", "stop", container])
             token_experiments = "bayes,ansiedad_matematica,comprension_lectora,crtnum,crt_verbal,graph_literacy,habilidad_matematica,matrices,memoria_funcional"
             rows.append([row[0], token + "[revoked]"])
         else:
@@ -78,7 +82,10 @@ else:
 if(token == ""):
     print "Study ended"
 else:
-    print("Copy this token: " + token)
+    clipboard = gtk.clipboard_get()
+    clipboard.set_text(token)
+    clipboard.store()
+    print("Token copied to clipboard.")
     error_file = open("run_incomplete","w")
     error_file.write(token + " " + token_experiments)
     error_file.close()
@@ -86,5 +93,6 @@ else:
     print("Starting experiment.")
     container = subprocess.check_output(["docker", "run", "-d", "-p", "80:80", "-v", os.path.realpath("") + "data:/scif/data/",image, "--headless", "--no-randomize", "--experiments",token_experiments, "start"])[:-1]
     print("Experiment started, press Control+C once it's ended.")
+    webbrowser.open_new("http://localhost")
     while(True):
         time.sleep(1000)
