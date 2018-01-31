a5178fcfb3b6c55f2f3c0f5bb7bb63de380f691d
Nicomero <nicolas.alarcon.14@sansano.usm.cl>
2018-01-31 16:37:10 -0300

    survey 364929

--- personalidad/experiment.js
+++ personalidad/experiment.js
@@ -41,7 +41,7 @@ var surveyexplanation={
     type: "instructions",
     pages: ["<div class = centerbox>"+
            "<p class = center-block-text>"+
-           "Conteste las siguientes preguntas sobre fe religiosa seg&uacute;n la siguiente escala"+
+           "Trabaje rápidamente y no piense demasiado en el significado exacto de las preguntas"+
            "</p></div>"],
     allow_keys: false,
     show_clickable_nav: true,
@@ -49,62 +49,181 @@ var surveyexplanation={
     data:{trialid: "Instructions_survey"}
 };
 
-var likert_scale = ["Fuertemente en desacuerdo","En desacuerdo","De acuerdo","Fuertemente de acuerdo"];
+var likert_scale = ["Casi nunca","Poco","Más o menos","Seguido","Casi siempre"];
 
 var survey02 = {
   type: "survey-multi-choice1",
-  questions: [{prompt: "<div class='justified'><br />Mi fe religiosa es extremadamente importante para m&iacute;.<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  questions: [{prompt: "<div class='justified'><br />¿Es una persona conversadora?<br /></div>", options: likert_scale, required: true, horizontal: true}],
   data: {trialid: "survey_02"}
 };
 
 var survey03 = {
   type: "survey-multi-choice1",
-  questions: [{prompt: "<div class='justified'><br />Rezo a diario.<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  questions: [{prompt: "<div class='justified'><br />¿Su estado de ánimo sufre altibajos con frecuencia?<br /></div>", options: likert_scale, required: true, horizontal: true}],
   data: {trialid: "survey_03"}
 };
 
 var survey04 = {
   type: "survey-multi-choice1",
-  questions: [{prompt: "<div class='justified'><br />Consireo que mi fe le otorga significado y prop&oacute;sito a mi vida.<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  questions: [{prompt: "<div class='justified'><br />¿Es usted una persona más bien animada o vital?<br /></div>", options: likert_scale, required: true, horizontal: true}],
   data: {trialid: "survey_04"}
 };
 
 var survey05 = {
   type: "survey-multi-choice1",
-  questions: [{prompt: "<div class='justified'><br />Me considero activo en mi fe o iglesia.<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  questions: [{prompt: "<div class='justified'><br />¿Se siente a veces desdichado sin motivo?<br /></div>", options: likert_scale, required: true, horizontal: true}],
   data: {trialid: "survey_05"}
 };
 
 var survey06 = {
   type: "survey-multi-choice1",
-  questions: [{prompt: "<div class='justified'><br />Mi fe es parte importante de qui&eacute;n soy como persona.<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  questions: [{prompt: "<div class='justified'><br />¿Tiene muchos amigos?<br /></div>", options: likert_scale, required: true, horizontal: true}],
   data: {trialid: "survey_06"}
 };
 
 var survey07 = {
   type: "survey-multi-choice1",
-  questions: [{prompt: "<div class='justified'><br />Mi relaci&oacute;n con Dios es extremadamente importante para m&iacute;.<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  questions: [{prompt: "<div class='justified'><br />¿Es una persona irritable?<br /></div>", options: likert_scale, required: true, horizontal: true}],
   data: {trialid: "survey_06"}
 };
 
 var survey08 = {
   type: "survey-multi-choice1",
-  questions: [{prompt: "<div class='justified'><br />Disfruto estando al rededor de otras personas que comparten mi fe.<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  questions: [{prompt: "<div class='justified'><br />¿Habitualmente, es capaz de liberarse y disfrutar en una fiesta animada?<br /></div>", options: likert_scale, required: true, horizontal: true}],
   data: {trialid: "survey_06"}
 };
 
 var survey09 = {
   type: "survey-multi-choice1",
-  questions: [{prompt: "<div class='justified'><br />Veo mi fe como una fuente de comodidad.<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  questions: [{prompt: "<div class='justified'><br />¿Se siente fácilmente herido en sus sentimientos?<br /></div>", options: likert_scale, required: true, horizontal: true}],
   data: {trialid: "survey_06"}
 };
 
 var survey10 = {
   type: "survey-multi-choice1",
-  questions: [{prompt: "<div class='justified'><br />Mi fe influye en muchas de mis decisiones.<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  questions: [{prompt: "<div class='justified'><br />¿Generalmente toma iniciativas al hacer nuevas amistades?<br /></div>", options: likert_scale, required: true, horizontal: true}],
   data: {trialid: "survey_06"}
 };
 
+var survey11 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br />¿A menudo siente harto(a) o hastiado(a)?<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey12 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br />¿Puede animar fácilmente una fiesta aburrida?<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey13 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br />¿Diría de sí mismo que es una persona tensa o muy nerviosa?<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey14 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br />¿Tiende a mantenerse apartado(a) en las situaciones sociales?<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey15 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br />¿Se preocupa a menudo por cosas que no debería haber hecho o dicho?<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey16 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br />¿Le gusta mezclarse con la gente?<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey17 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br />¿Diría de sí mismo que es una persona nerviosa?<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey18 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br />¿Le gusta el bullicio y la agitación a su alrededor?<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey19 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br />¿Después de una experiencia embarazosa, se siente preocupado durante mucho tiempo?<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey20 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br />Por lo general, ¿suele estar callado/a cuando esta con otras personas?<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey21 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br />¿Sufre de los “nervios”?<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey22 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br />¿La gente piensa que usted es una persona animada?<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey23 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br />¿A menudo se siente solo?<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey24 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br />¿Puede organizar y conducir una fiesta?<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey25 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br />¿Tiene a menudo sentimientos de culpabilidad?<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey26 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br />¿Le gusta contar chistes o historias divertidas a sus amigos?<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey27 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br />¿Es usted una persona que sufre con facilidad?<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey28 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br />¿Le gusta salir a menudo?<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey29 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br />¿Se siente a menudo apático/a y cansado/a sin motivo?<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey30 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br />¿A menudo siente que la vida es muy monótona?<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
 
 
 // Creacion de timeline e inclusion de trials
@@ -133,3 +252,23 @@ personalidad_experiment.push(survey07);
 personalidad_experiment.push(survey08);
 personalidad_experiment.push(survey09);
 personalidad_experiment.push(survey10);
+personalidad_experiment.push(survey11);
+personalidad_experiment.push(survey12);
+personalidad_experiment.push(survey13);
+personalidad_experiment.push(survey14);
+personalidad_experiment.push(survey15);
+personalidad_experiment.push(survey16);
+personalidad_experiment.push(survey17);
+personalidad_experiment.push(survey18);
+personalidad_experiment.push(survey19);
+personalidad_experiment.push(survey20);
+personalidad_experiment.push(survey21);
+personalidad_experiment.push(survey22);
+personalidad_experiment.push(survey23);
+personalidad_experiment.push(survey24);
+personalidad_experiment.push(survey25);
+personalidad_experiment.push(survey26);
+personalidad_experiment.push(survey27);
+personalidad_experiment.push(survey28);
+personalidad_experiment.push(survey29);
+personalidad_experiment.push(survey30);
