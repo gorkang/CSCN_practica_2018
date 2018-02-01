29eedcaed43275b0901f514e71ae874d247fe1e3
Nicomero <nicolas.alarcon.14@sansano.usm.cl>
2018-02-01 16:19:36 -0300

    initial files

---
+++ vocabulario/experiment.js
@@ -0,0 +1,82 @@
+/**
+@file
+@name
+ * The experiment 427728
+ *
+ * CSCN lab
+ *
+ */
+
+/**
+Blocks f1 and f5
+@name block_fkeys
+@function
+@param {event}  event
+
+*/
+
+onkeydown = function block_fkeys(event){
+    var x = event.which || event.keyCode;
+    if(x == 112 || x == 116){
+        console.log("Blocked key");
+        event.preventDefault();
+        return false;
+    }else{
+        return;
+    }
+}
+
+var screen_vocabulario_experiment = {
+    type: 'instructions',
+    pages: ['<p><left>vocabulario<br /></p>'],
+    data:{trialid: "Screen_WM"},
+    show_clickable_nav: true,
+    on_trial_start: function(){
+        bloquear_enter = 0;
+    }
+};
+
+// Inicio prueba
+var surveyexplanation={
+    type: "instructions",
+    pages: ["<div class = centerbox>"+
+           "<p class = center-block-text>"+
+           "Diga a qué corresponden las figuras"+
+           "</p></div>"],
+    allow_keys: false,
+    show_clickable_nav: true,
+    timing_post_trial: 50,
+    data:{trialid: "Instructions_survey"}
+};
+
+var img1 = {
+  type: "survey-text",
+  preamble: " Diga a qué corresponden la figura<br /><br />",
+  questions: [{
+    prompt: " <img src='images/auto.jpeg' alt='Smiley face' height='42' width='42'> "
+  }],
+  data: {
+    trialid: "VCRT_05"
+  }
+};
+
+
+
+// Creacion de timeline e inclusion de trials
+vocabulario_experiment = [];    //timeline
+
+//if the experiment isn't in fullscreen, add trial to make it fullscreen
+if(window.innerWidth != screen.width || window.innerHeight != screen.height){
+  vocabulario_experiment.push({
+    type: 'fullscreen',
+    message: '<p>El experimento entrara en modo pantalla completa</p>',
+    button_label: "Pantalla Completa",
+    delay_after: 0,
+    fullscreen_mode: true
+  });
+}
+
+//add the trials to the timeline
+vocabulario_experiment.push(screen_vocabulario_experiment);
+vocabulario_experiment.push(surveyexplanation);
+vocabulario_experiment.push(img1);
