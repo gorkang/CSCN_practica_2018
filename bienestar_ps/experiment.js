615ce6bfa4b3db7b7078901ba04aa46630b97e43
Nicomero <nicolas.alarcon.14@sansano.usm.cl>
2018-02-01 13:04:50 -0300

    initial files

---
+++ bienestar_ps/experiment.js
@@ -0,0 +1,279 @@
+/**
+@file
+@name
+ * The experiment 472983
+ *
+ * CSCN lab
+ *
+ */
+
+/**
+Blocks f1 and f5
+@name block_fkeys
+@function
+@param {event}  event
+
+*/
+
+onkeydown = function block_fkeys(event){
+    var x = event.which || event.keyCode;
+    if(x == 112 || x == 116){
+        console.log("Blocked key");
+        event.preventDefault();
+        return false;
+    }else{
+        return;
+    }
+}
+
+var screen_bienestar_ps_experiment = {
+    type: 'instructions',
+    pages: ['<p><left>bienestar_ps<br /></p>'],
+    data:{trialid: "Screen_WM"},
+    show_clickable_nav: true,
+    on_trial_start: function(){
+        bloquear_enter = 0;
+    }
+};
+
+// Inicio prueba
+var surveyexplanation={
+    type: "instructions",
+    pages: ["<div class = centerbox>"+
+           "<p class = center-block-text>"+
+           "Las siguientes declaraciones indagan sobre sus pensamientos y sentimientos en una variedad de situaciones. Para cada ítem, indique cuán bien la declaración lo/a describe."+
+           "</p></div>"],
+    allow_keys: false,
+    show_clickable_nav: true,
+    timing_post_trial: 50,
+    data:{trialid: "Instructions_survey"}
+};
+
+var likert_scale = ["A <br> <p style='font-size:10px'>No me <br>describe bien</p> ","B","C","D","E <br> <p style='font-size:10px'>Me describe <br> muy bien</p>"];
+
+var survey02 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_02"}
+};
+
+var survey03 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_03"}
+};
+
+var survey04 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_04"}
+};
+
+var survey05 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_05"}
+};
+
+var survey06 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey07 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey08 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey09 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey10 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey11 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey12 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey13 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey14 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey15 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey16 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /></text><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey17 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey18 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey19 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey20 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey21 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey22 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey23 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey24 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey25 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br />Tiendo a perder el control en las emergencias.<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey26 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey27 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey28 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey29 = {
+  type: "survey-multi-choice1",
+  questions: [{prompt: "<div class='justified'><br /><br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+
+
+// Creacion de timeline e inclusion de trials
+bienestar_ps_experiment = [];    //timeline
+
+//if the experiment isn't in fullscreen, add trial to make it fullscreen
+if(window.innerWidth != screen.width || window.innerHeight != screen.height){
+  bienestar_ps_experiment.push({
+    type: 'fullscreen',
+    message: '<p>El experimento entrara en modo pantalla completa</p>',
+    button_label: "Pantalla Completa",
+    delay_after: 0,
+    fullscreen_mode: true
+  });
+}
+
+//add the trials to the timeline
+bienestar_ps_experiment.push(screen_bienestar_ps_experiment);
+bienestar_ps_experiment.push(surveyexplanation);
+bienestar_ps_experiment.push(survey02);
+bienestar_ps_experiment.push(survey03);
+bienestar_ps_experiment.push(survey04);
+bienestar_ps_experiment.push(survey05);
+bienestar_ps_experiment.push(survey06);
+bienestar_ps_experiment.push(survey07);
+bienestar_ps_experiment.push(survey08);
+bienestar_ps_experiment.push(survey09);
+bienestar_ps_experiment.push(survey10);
+bienestar_ps_experiment.push(survey11);
+bienestar_ps_experiment.push(survey12);
+bienestar_ps_experiment.push(survey13);
+bienestar_ps_experiment.push(survey14);
+bienestar_ps_experiment.push(survey15);
+bienestar_ps_experiment.push(survey16);
+bienestar_ps_experiment.push(survey17);
+bienestar_ps_experiment.push(survey18);
+bienestar_ps_experiment.push(survey19);
+bienestar_ps_experiment.push(survey20);
+bienestar_ps_experiment.push(survey21);
+bienestar_ps_experiment.push(survey22);
+bienestar_ps_experiment.push(survey23);
+bienestar_ps_experiment.push(survey24);
+bienestar_ps_experiment.push(survey25);
+bienestar_ps_experiment.push(survey26);
+bienestar_ps_experiment.push(survey27);
+bienestar_ps_experiment.push(survey28);
+bienestar_ps_experiment.push(survey29);
+bienestar_ps_experiment.push(survey30);
+bienestar_ps_experiment.push(survey31);
+bienestar_ps_experiment.push(survey32);
+bienestar_ps_experiment.push(survey33);
+bienestar_ps_experiment.push(survey34);
+bienestar_ps_experiment.push(survey35);
+bienestar_ps_experiment.push(survey36);
+bienestar_ps_experiment.push(survey37);
+bienestar_ps_experiment.push(survey38);
+bienestar_ps_experiment.push(survey39);
+bienestar_ps_experiment.push(survey40);
