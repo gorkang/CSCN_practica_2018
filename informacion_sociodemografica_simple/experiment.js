4d38a3cb64c9ea3cb975d16868fc07354f94ab7f
Nicomero <nicolas.alarcon.14@sansano.usm.cl>
2018-02-05 14:59:57 -0300

    initial files

---
+++ informacion_sociodemografica_simple/experiment.js
@@ -0,0 +1,71 @@
+/**
+@file
+@name
+ * The experiment 279271
+ *
+ * CSCN lab
+ *
+ */
+
+/**
+Blocks f1 and f5
+@name block_fkeys
+@function
+@param {event}  event
+
+*/
+
+onkeydown = function block_fkeys(event){
+    var x = event.which || event.keyCode;
+    if(x == 112 || x == 116){
+        console.log("Blocked key");
+        event.preventDefault();
+        return false;
+    }else{
+        return;
+    }
+}
+
+var screen_informacion_sociodemografica_simple_experiment = {
+    type: 'instructions',
+    pages: ['<p><left>informacion_sociodemografica_simple<br /></p>'],
+    data:{trialid: "Screen_WM"},
+    show_clickable_nav: true,
+    on_trial_start: function(){
+        bloquear_enter = 0;
+    }
+};
+
+// Inicio prueba
+var surveyexplanation={
+    type: "instructions",
+    pages: ["<div class = centerbox>"+
+           "<p class = center-block-text>"+
+           "Conteste las siguientes preguntas sobre fe religiosa seg&uacute;n la siguiente escala"+
+           "</p></div>"],
+    allow_keys: false,
+    show_clickable_nav: true,
+    timing_post_trial: 50,
+    data:{trialid: "Instructions_survey"}
+};
+
+
+
+
+// Creacion de timeline e inclusion de trials
+informacion_sociodemografica_simple_experiment = [];    //timeline
+
+//if the experiment isn't in fullscreen, add trial to make it fullscreen
+if(window.innerWidth != screen.width || window.innerHeight != screen.height){
+  informacion_sociodemografica_simple_experiment.push({
+    type: 'fullscreen',
+    message: '<p>El experimento entrara en modo pantalla completa</p>',
+    button_label: "Pantalla Completa",
+    delay_after: 0,
+    fullscreen_mode: true
+  });
+}
+
+//add the trials to the timeline
+informacion_sociodemografica_simple_experiment.push(screen_informacion_sociodemografica_simple_experiment);
+informacion_sociodemografica_simple_experiment.push(surveyexplanation);
