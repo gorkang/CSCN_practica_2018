7cf6c9dcdd8a0eb3b060932fa854b29ce1139e08
Nicomero <nicolas.alarcon.14@sansano.usm.cl>
2018-02-21 14:44:27 -0300

    local d3 bayes

--- pruebas_individuales/bayes/experiment.js
+++ pruebas_individuales/bayes/experiment.js
@@ -7,23 +7,6 @@
  *
  */
 
-/**
-Blocks f1 and f5
-@name block_fkeys
-@function
-@param {event}  event
-
-*/
-onkeydown = function block_fkeys(event) {
-    var x = event.which || event.keyCode;
-    if (x == 112 || x == 116) {
-        console.log("Blocked key");
-        event.preventDefault();
-        return false;
-    } else {
-        return;
-    }
-}
 
 var ide = 1;
 
