0a78393d98aac55879dc71091acc8b69b34dad33
AlejandraNeely <aleneely@gmail.com>
2019-01-30 16:58:00 -0300

    CRT_MCQ4 to CRTMCQ4_cognitive-reflection-test-extended, documentation added CRTv_cognitive_reflection_verbal to CRTv_cognitive-reflection-test-verbal, no documentation added, could not find a verbal cognitive reflection test in the literature CRT_cognitive-reflection-test no documentation added, could not find the spanish version of this test DSS_decision_styles_scale to DSS_decision-styles-scale, documentation added ECR-RS_relationship_structures to ECRRS_experiences-in-close-relationship-structures, documentation added (spanish and english) EPI_eysenck_personality_inventory to EPI_eysenck-personality-inventory, documentation added (spanish and english) escala_experiencias_disociativas to DES_dissociative-experiences-scale, documentation added (spanish and english) escala_rosenberg to RSE_rosenberg-self-esteem, documentation added (english and spanish) escala_tacticas to CTS2_conflict-tactics-scale, documentation added (english and spanish) BRET_bomb_risk_elicitation_task to BRET_bomb-risk-elicitation-task, documentation added estilo_atribucional to LCS_locus-of-control-scale, documentation added (english and spanish)

---
+++ pruebas_individuales/BRET_bomb-risk-elicitation-task/experiment.js
@@ -0,0 +1,141 @@
+onkeydown = function block_fkeys(event) {
+  var x = event.which || event.keyCode;
+  if (x == 112 || x == 116) {
+    console.log("Blocked key");
+    event.preventDefault();
+    return false;
+  } else {
+    return;
+  }
+}
+
+var score_total;
+repetitions = 2;
+
+/* ********************************* PANTALLAS DE INICIO Y DESPEDIDA ********************************* */
+
+var welcome = {
+  type: 'instructions',
+  pages: ['<p><left>BRET<br /></p>'],
+  data: {
+    trialId: "Screen_BRET"
+  },
+  show_clickable_nav: true
+};
+
+var instructions_test = {
+  type: 'instructions',
+  pages: ['Instrucciones para test'],
+  data: {
+    trialId: "Screen_BRET"
+  },
+  show_clickable_nav: true
+};
+
+var bret_trial_0 = {
+  type: 'bret',
+  title: 'Trial de practica',
+  data: {
+    trialId: "bret_test"
+  },
+  reveal_bombs: "never",
+  allow_clicking: false,
+  auto_advance_time: 1
+}
+
+var instructions_after_test = {
+  type: 'instructions',
+  pages: ['Instrucciones para experimento'],
+  data: {
+    trialId: "Screen_BRET"
+  },
+  show_clickable_nav: true
+};
+
+var bret_trial_1 = {
+  type: 'bret',
+  title: 'Trial 1',
+  data: {
+    trialId: "bret_1"
+  },
+  reveal_bombs: "at_click",
+  allow_clicking: false,
+  auto_advance_time: 1,
+  on_finish: function(data) {
+    score_total = data.score;
+  }
+}
+
+var bret_total_score_1= {
+  type: 'instructions',
+  pages: [],
+  data: {
+    trialId: "score_total_1"
+  },
+  show_clickable_nav: true,
+  on_start: function(trial) {
+    trial.pages = ["Total acumulado: " + score_total.toFixed(2)];
+  }
+}
+
+var goodbye = {
+  type: 'instructions',
+  pages: ['<p><left>Ha completado BRET<br /></p>'],
+  data: {
+    trialId: "goodbye"
+  },
+  show_clickable_nav: true
+};
+/* ********************************* INICIALIZACION DE EXPERIMENTO ********************************* */
+
+bret = []; //timeline
+
+//if the experiment isn't in fullscreen, add trial to make it fullscreen
+if (window.innerWidth != screen.width || window.innerHeight != screen.height) {
+  bret.push({
+    type: 'fullscreen',
+    message: '<p>El experimento entrara en modo pantalla completa</p>',
+    button_label: "Pantalla Completa",
+    delay_after: 0,
+    fullscreen_mode: true
+  });
+}
+
+bret.push(welcome);
+bret.push(instructions_test);
+bret.push(bret_trial_0);
+bret.push(instructions_after_test);
+bret.push(bret_trial_1);
+bret.push(bret_total_score_1);
+index = 2;
+while (repetitions > 0) {
+  repetitions -= 1;
+  bret.push({
+    type: 'bret',
+    title: 'Trial ' + index,
+    data: {
+      trialId: "bret_" + index
+    },
+    reveal_bombs: "at_click",
+    allow_clicking: false,
+    auto_advance_time: 1,
+    on_finish: function(data) {
+      score_total += data.score;
+    }
+  });
+
+  bret.push({
+    type: 'instructions',
+    pages: [],
+    data: {
+      trialId: "score_total_" + index
+    },
+    show_clickable_nav: true,
+    on_start: function(trial) {
+      trial.pages = ["Total acumulado: " + score_total.toFixed(2)];
+    }
+  })
+  index += 1;
+}
+
+bret.push(goodbye);
---
+++ pruebas_individuales/RSE_rosenberg-self-esteem/experiment.js
@@ -0,0 +1,134 @@
+/**
+@file
+@name
+ * The experiment 387487
+ *
+ * CSCN lab
+ *
+ */
+
+/**
+Blocks f1 and f5
+@name block_fkeys
+@function
+@param {event}  event
+
+*/
+
+onkeydown = function block_fkeys(event){
+    var x = event.which || event.keyCode;
+    if(x == 112 || x == 116){
+        console.log("Blocked key");
+        event.preventDefault();
+        return false;
+    }else{
+        return;
+    }
+}
+
+var screen_escala_rosenberg_experiment = {
+    type: 'instructions',
+    pages: ['<p><left><b><big>Escala rosenberg</big></b><br />'+
+    "A continuaci&oacute;n encontrar&aacute; una lista de afirmaciones en torno a los "+
+     "sentimientos o pensamientos que tiene sobre usted mismo/a. Marque "+
+     "la respuesta que m&aacute;s lo identifica."+
+    '</p>'],
+    data:{trialid: "Screen_WM"},
+    show_clickable_nav: true,
+    on_trial_start: function(){
+        bloquear_enter = 0;
+    }
+};
+
+
+var likert_scale = ["Muy de acuerdo","De acuerdo","En desacuerdo","Muy en desacuerdo"];
+
+var survey01 = {
+  type: "survey-multi-choice-horizontal",
+  questions: [{prompt: "<div class='justified'><br />Siento que soy una persona digna de aprecio, al menos en igual medida que los dem&aacute;s.<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_01"}
+};
+
+var survey02 = {
+  type: "survey-multi-choice-horizontal",
+  questions: [{prompt: "<div class='justified'><br />Creo que tengo un buen n&uacute;mero de cualidades.<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_02"}
+};
+
+var survey03 = {
+  type: "survey-multi-choice-horizontal",
+  questions: [{prompt: "<div class='justified'><br />En general me inclino a pensar que soy un/a fracasado/a.<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_03"}
+};
+
+var survey04 = {
+  type: "survey-multi-choice-horizontal",
+  questions: [{prompt: "<div class='justified'><br />Soy capaz de hacer las cosas tan bien como la mayor&iacute;a de la gente.<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_04"}
+};
+
+var survey05 = {
+  type: "survey-multi-choice-horizontal",
+  questions: [{prompt: "<div class='justified'><br />Siento que no tengo muchos motivos para sentirme orgulloso/a de mi.<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_05"}
+};
+
+var survey06 = {
+  type: "survey-multi-choice-horizontal",
+  questions: [{prompt: "<div class='justified'><br />Tengo una actitud positiva hacia mi mismo/a.<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_06"}
+};
+
+var survey07 = {
+  type: "survey-multi-choice-horizontal",
+  questions: [{prompt: "<div class='justified'><br />En general, estoy satisfecho/a conmigo mismo/a.<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_07"}
+};
+
+var survey08 = {
+  type: "survey-multi-choice-horizontal",
+  questions: [{prompt: "<div class='justified'><br />Desear&iacute;a valorarme m&aacute;s a mi mismo/a.<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_08"}
+};
+
+var survey09 = {
+  type: "survey-multi-choice-horizontal",
+  questions: [{prompt: "<div class='justified'><br />A veces me siento verdaderamente in&uacute;til.<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_09"}
+};
+
+var survey10 = {
+  type: "survey-multi-choice-horizontal",
+  questions: [{prompt: "<div class='justified'><br />A veces pienso que no soy bueno/a para nada.<br /></div>", options: likert_scale, required: true, horizontal: true}],
+  data: {trialid: "survey_10"}
+};
+
+
+
+// Creacion de timeline e inclusion de trials
+escala_rosenberg_experiment = [];    //timeline
+
+//if the experiment isn't in fullscreen, add trial to make it fullscreen
+if(window.innerWidth != screen.width || window.innerHeight != screen.height){
+  escala_rosenberg_experiment.push({
+    type: 'fullscreen',
+    message: '<p>El experimento entrara en modo pantalla completa</p>',
+    button_label: "Pantalla Completa",
+    delay_after: 0,
+    fullscreen_mode: true
+  });
+}
+
+//add the trials to the timeline
+escala_rosenberg_experiment.push(screen_escala_rosenberg_experiment);
+
+escala_rosenberg_experiment.push(survey01);
+escala_rosenberg_experiment.push(survey02);
+escala_rosenberg_experiment.push(survey03);
+escala_rosenberg_experiment.push(survey04);
+escala_rosenberg_experiment.push(survey05);
+escala_rosenberg_experiment.push(survey06);
+escala_rosenberg_experiment.push(survey07);
+escala_rosenberg_experiment.push(survey08);
+escala_rosenberg_experiment.push(survey09);
+escala_rosenberg_experiment.push(survey10);
